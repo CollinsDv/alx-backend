6b721c17858f7f6d2a9feb9ad7b0043c6e41bc62
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-18 12:22:11 +0300

    handle correct data handle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -20,6 +20,6 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     if isinstance(page, int) and isinstance(page_size, int) and\
             page > 0 and page_size > 0:
         start_index = (page - 1) * page_size
-        end_index = page + page_size
+        end_index = page * page_size
 
         return (start_index, end_index)
