2a9cf3cd538bf45c5dc61807f9593fc91415bac3
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-20 21:12:54 +0300

    handle PEP

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,16 +3,19 @@
 module 0-basic_cache
 involves implementing the put and get method from base class
 
-Create a class BasicCache that inherits from BaseCaching and is a caching system:
+Create a class BasicCache that inherits from BaseCaching and is a caching
+system:
 
 * You must use self.cache_data - dictionary from the parent class BaseCaching
 * This caching system doesn’t have limit
 * def put(self, key, item):
-	* Must assign to the dictionary self.cache_data the item value for the key key.
-	* If key or item is None, this method should not do anything.
+    * Must assign to the dictionary self.cache_data the item value for the
+      key key.
+    * If key or item is None, this method should not do anything.
 * def get(self, key):
-	* Must return the value in self.cache_data linked to key.
-	* If key is None or if the key doesn’t exist in self.cache_data, return None.
+    * Must return the value in self.cache_data linked to key.
+    * If key is None or if the key doesn’t exist in self.cache_data, return
+      None.
 """
 from typing import Any, Union
 BaseCaching = __import__('base_caching').BaseCaching
