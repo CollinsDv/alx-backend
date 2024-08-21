c464b4a52132ffa2a488c0e7f70430e01ef9dd98
Collins Kimani <mwas9446.ck@gmail.com>
2024-08-21 11:25:41 +0300

    handle PEP coding style

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 """
 module 1-fifo_cache
-Create a class FIFOCache that inherits from BaseCaching and is a caching system:
+Create a class FIFOCache that inherits from BaseCaching
+and is a caching system:
 
-    You must use self.cache_data - dictionary from the parent class BaseCaching
-    You can overload def __init__(self): but don’t forget to call the parent init: super().__init__()
+    You must use self.cache_data - dictionary from the
+    parent class BaseCaching
+    You can overload def __init__(self): but don’t forget
+    to call the parent init: super().__init__()
     def put(self, key, item):
-      Must assign to the dictionary self.cache_data the item value for the key key.
-      If key or item is None, this method should not do anything.
-      If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-        you must discard the first item put in cache (FIFO algorithm)
-        you must print DISCARD: with the key discarded and following by a new line
+      - Must assign to the dictionary self.cache_data the item
+      value for the key key.
+      - If key or item is None, this method should not do anything.
+      - If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS:
+        - you must discard the first item put in cache (FIFO algorithm)
+        - you must print DISCARD: with the key discarded and following
+          by a new line
     def get(self, key):
-      Must return the value in self.cache_data linked to key.
-      If key is None or if the key doesn’t exist in self.cache_data, return None.
+      - Must return the value in self.cache_data linked to key.
+      - If key is None or if the key doesn’t exist in self.cache_data,
+        return None.
 """
 from typing import Any, Union
 from collections import deque
@@ -38,7 +45,7 @@ class FIFOCache(BaseCaching):
         if key and item:
             if len(self.cache_data) < type(self).MAX_ITEMS:
                 # add item to cache and queue
-                self.queue.append(key) 
+                self.queue.append(key)
                 self.cache_data[key] = item
             else:
                 # check for cache misses
@@ -54,7 +61,7 @@ class FIFOCache(BaseCaching):
                     # handle cache hit
                     if self.cache_data[key] is not item:
                         self.cache_data[key] = item
-            
+
     def get(self, key: Any) -> Union[Any, None]:
         """get a value in cache
         Args:
